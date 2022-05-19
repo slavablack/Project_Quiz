25dc92d5f248c1ad094ae1b0382347c75d4c80f3
slavablack <veaceslav.ciornii@magenta.de>
2022-05-19 13:25:00 +0200

    post route created

--- src/index.js
+++ src/index.js
@@ -11,16 +11,44 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
+// const connect = process.env.DB_LOCALCONNECTION;
+const url = process.env.DB_REMOTTE;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const quizSchema = new mongoose.Schema({
+  Frage: String,
+  antwort: Array,
+  correct: Number,
+});
+const Quiz = mongoose.model("QuizApp", quizSchema, "quizquestions");
+
+const getQuestions = async () => {
+  return await Quiz.find().exec();
+};
+app.get("/", async (req, res) => {
+  try {
+    const data = await getQuestions();
+    res.json(data);
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+app.post("/newGamer", async (req, res) => {
+  try {
+    const newGamer = new Quiz({
+      ...req.body,
+    });
+    const newSpieler = await newGamer.save();
+    res.json(newSpieler);
+  } catch (err) {
+    res.json({ err: err.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-// mongoose.connect(url).then(() => {
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//   });
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
 // });
+mongoose.connect(url).then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+});
